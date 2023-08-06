bb610a73be1bc0204d016a5c0bcc55ee01a60b01
Rayene <rallouan@student.42heilbronn.de>
2023-08-06 16:03:56 +0200

    Added moves init operations

---
+++ src/moves_op.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves_op.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rallouan <rallouan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 16:23:32 by rallouan          #+#    #+#             */
+/*   Updated: 2023/07/30 16:02:40 by rallouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+
+t_moves	*create_move(char *move)
+{
+	t_moves	*m;
+
+	m = malloc(sizeof(*m));
+	if (!m)
+		return (0);
+	m->move = move;
+	m->next = NULL;
+	m->prev = NULL;
+	return (m);
+}
+
+void	ft_add_move(t_moves **moves, t_moves *new)
+{
+	t_moves		*temp;
+
+	if (!new)
+		return ;
+	if (!(*moves))
+		(*moves) = new;
+	else
+	{
+		temp = (*moves);
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+		new->prev = temp;
+	}
+}
+
+void	remove_move(t_moves **moves, t_moves *m)
+{
+	t_moves	*current;
+
+	if (!moves || !(*moves))
+		return ;
+	current = *moves;
+	while (current && (current != m))
+		current = current->next;
+	if (! current)
+		return ;
+	if (current == (*moves))
+	{
+		(*moves) = current->next;
+		if (*moves)
+			(*moves)->prev = NULL;
+	}
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next)
+			current->next->prev = current->prev;
+	}
+	free (current);
+}
+
+void	ft_print_moves(t_moves *moves)
+{
+	printf("Moves values: ");
+	while (moves)
+	{
+		printf("%s ", moves->move);
+		moves = moves->next;
+	}
+	printf("\n");
+}
+
+void	ft_free_moves(t_moves **moves)
+{
+	t_moves	*tmp;
+
+	if (!moves || !(*moves))
+		return ;
+	tmp = *moves;
+	while (*moves)
+	{
+		tmp = (*moves)->next;
+		free(*moves);
+		*moves = tmp;
+	}
+}
