d6bc7a92bd577197128d3c6334a1ca03eeebb208
Rayene <rallouan@student.42heilbronn.de>
2023-08-06 16:02:29 +0200

    Added rotate functions

---
+++ src/rotate.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rallouan <rallouan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/30 12:01:00 by rallouan          #+#    #+#             */
+/*   Updated: 2023/07/30 14:30:11 by rallouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//Make the list circular
+//Shift head by one position forward
+//Makes the list non-circular again
+void	rotate(t_stack **stack)
+{
+	t_stack	*temp;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+		return ;
+	temp = (*stack);
+	while (temp->next)
+		temp = temp->next;
+	temp->next = (*stack);
+	(*stack)->prev = temp;
+	(*stack) = (*stack)->next;
+	temp = temp->next;
+	temp->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+void	rotate_a(t_stack **stack_a, t_moves **moves)
+{
+	t_moves	*m;
+
+	rotate(stack_a);
+	m = create_move("ra");
+	ft_add_move(moves, m);
+}
+
+void	rotate_b(t_stack **stack_b, t_moves **moves)
+{
+	t_moves	*m;
+
+	rotate(stack_b);
+	m = create_move("rb");
+	ft_add_move(moves, m);
+}
+
+void	rotate_both(t_stack **stack_a, t_stack **stack_b, t_moves **moves)
+{
+	t_moves	*m;
+
+	rotate(stack_a);
+	rotate(stack_b);
+	m = create_move("rr");
+	ft_add_move(moves, m);
+}
