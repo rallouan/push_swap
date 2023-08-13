554fe915eed1ba4e81069c7ecbca84f0042c88f7
Rayene <rallouan@student.42heilbronn.de>
2023-08-13 17:01:55 +0200

    rev_rotate error fix

--- src/rev_rotate.c
+++ src/rev_rotate.c
@@ -6,11 +6,11 @@
 /*   By: rallouan <rallouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 14:09:46 by rallouan          #+#    #+#             */
-/*   Updated: 2023/07/30 14:30:06 by rallouan         ###   ########.fr       */
+/*   Updated: 2023/08/12 14:20:21 by rallouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
 
 // Make it circular then shift by one back (last elt becomes first) the unlink
 void	rev_rotate(t_stack **stack)
@@ -39,11 +39,11 @@ void	rev_rotate_a(t_stack **stack_a, t_moves **moves)
 	ft_add_move(moves, m);
 }
 
-void	rev_rotate_b(t_stack **stack_a, t_moves **moves)
+void	rev_rotate_b(t_stack **stack_b, t_moves **moves)
 {
 	t_moves	*m;
 
-	rev_rotate(stack_a);
+	rev_rotate(stack_b);
 	m = create_move("rrb");
 	ft_add_move(moves, m);
 }
