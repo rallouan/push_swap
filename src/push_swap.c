0c14455df4d19c5715bd3e519f04be1f55210606
Rayene <rallouan@student.42heilbronn.de>
2023-08-13 17:46:03 +0200

    main push_swap

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,28 +6,29 @@
 /*   By: rallouan <rallouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 13:03:45 by rallouan          #+#    #+#             */
-/*   Updated: 2023/08/06 15:52:41 by rallouan         ###   ########.fr       */
+/*   Updated: 2023/08/13 17:45:35 by rallouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
 
 int	main(int argc, char **argv)
 {
-	t_stack		**stack_a;
-	t_stack		**stack_b;
-	t_moves		**moves;
+	t_stack		*stack_a;
+	t_stack		*stack_b;
+	t_moves		*moves;
 	int			size;
 
 	stack_b = NULL;
+	stack_a = NULL;
 	moves = NULL;
-	stack_a = parse_args(argc, argv, stack_a);
-	size = ft_stack_size(*stack_a);
+	stack_a = parse_args(argc, argv, &stack_a);
+	size = ft_stack_size(stack_a);
 	ft_indexation(stack_a, size + 1);
-	sort(stack_a, stack_b, moves, size);
-	ft_print_moves(*moves);
-	ft_free_stack(stack_a);
-	ft_free_stack(stack_b);
-	ft_free_moves(moves);
+	sort(&stack_a, &stack_b, &moves, size);
+	ft_print_moves(moves);
+	ft_free_stack(&stack_a);
+	ft_free_stack(&stack_b);
+	ft_free_moves(&moves);
 	return (0);
 }
